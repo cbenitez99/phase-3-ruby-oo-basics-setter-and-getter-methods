7feb44d7fb11d41555f71c6d7ba9876459351ead
Cristian Benitez <cbenitezbusiness9@gmail.com>
2021-09-16 16:21:19 -0500

    commit tree de259a0b5296b23a808f1943936b9c5fd41d9c47

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    attr_accessor :name, :job
 
+    # def name=(name) writer
+    #     @name = name
+    # end
+
+    # def name reader
+    #     @name
+    # end
+
+    # def job=(job) writer
+    #     @job = job
+    # end
+
+    # def job reader
+    #     @job
+    # end
 end
